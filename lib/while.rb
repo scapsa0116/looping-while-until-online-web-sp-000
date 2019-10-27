d0f57d35a0a3c98326074731a6c9865ab49d77f7
Stela Capsa <stela.capsa@gmail.com>
2019-10-27 19:33:23 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,6 @@
 def using_while
   levitation_force = 6
-	
-  #your code here
-end
-
-
+  while levitation_force <10
+    puts "While your #{levitation_force} is less than 10"
+    levitation_force += 1
+  end
